70bce978d59c1fca2b104a408ce45c55ad2030d8
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2020-11-01 18:10:11 -0500

    let's try this again :)

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,7 +13,7 @@ exports.createPages = ({ graphql, actions }) => {
             allContentfulProduct {
               edges {
                 node {
-                  title
+                  name
                   slug
                 }
               }
