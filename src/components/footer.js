6ddf1361b803cdab6597e14dcbf409332b44e777
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2020-11-06 17:59:09 -0500

    finished footer :D

---
+++ src/components/footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Facebook from '../../static/facebook.svg'
+import Instagram from '../../static/instagram.svg'
+
+export default () => (
+  <div class="py-8 mx-auto bg-gray-300">
+    <div style={{ maxWidth: 1180, margin: '0 auto' }} class="flex px-5">
+      <div class="w-3/5 caption space-y-1">
+        <p class="font-semibold">Be the first</p>
+        <p>Want high quality lures crafted carefully by hand? </p>
+        <p>
+          Follow us on Facebook or Instagram to receive our latest products and
+          updates!
+        </p>
+      </div>
+      <div class="w-2/5 flex space-x-4 my-auto">
+        <a
+          href="https://www.facebook.com/FEROX-lures-102729374937442/"
+          class="ml-auto"
+          target="_blank"
+        >
+          <img src={Facebook} alt="Facebook logo" class="w-10" />
+        </a>
+        <a href="https://www.instagram.com/feroxlures/" target="_blank">
+          <img src={Instagram} alt="Instagram logo" class="w-10" />
+        </a>
+      </div>
+    </div>
+  </div>
+)
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Container from './container'
 import Navigation from './navigation'
+import Footer from './footer'
 
 class Template extends React.Component {
   render() {
     const { location, children } = this.props
-    let header
 
     let rootPath = `/`
     if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
@@ -14,10 +14,13 @@ class Template extends React.Component {
     }
 
     return (
-      <Container>
-        {/* <Navigation /> */}
-        <div class="px-5 py-8 mx-auto"> {children}</div>
-      </Container>
+      <>
+        <Container>
+          {/* <Navigation /> */}
+          <div class="px-5 py-8 mx-auto">{children}</div>
+        </Container>
+        <Footer />
+      </>
     )
   }
 }
